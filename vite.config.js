098f9c8e07e4d463d8a2949f0b469b67fd0049e6
Jein <djeinabou.bodie@gmail.com>
2026-04-27 13:36:51 -0700

    Style.css wasn't appearing in web portal because it didn't have the base setting in the vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const gameData = require('./data/game.json');
 
 export default defineConfig({
+  base: `/staticGames/${gameData['game-id']}/`,
   root: '.',
   build: {
     outDir: 'dist',
